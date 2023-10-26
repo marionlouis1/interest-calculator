a79c95799f1cdd58b471452d5b4d1e58923e06fd
marionlouis1 <marionlouis1@gmail.com>
2023-10-26 16:01:12 +0200

    first blog article fix

--- pages/blog.js
+++ pages/blog.js
@@ -2,7 +2,7 @@ import React from "react";
 import Head from "next/head";
 import Header from "../components/header";
 
-export default function Article() {
+export default function Blog() {
     return (
         <>
         <Head>
