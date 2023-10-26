f65b6ec71cdea37abdc6ca0b787948a7b8e0dc58
marionlouis1 <marionlouis1@gmail.com>
2023-10-26 16:29:14 +0200

    test half 1sentence in p3

--- pages/blog.js
+++ pages/blog.js
@@ -22,7 +22,8 @@ export default function Blog() {
         <main>
             <h1>The Hidden Treasure of Compound Interest: Start Investing Today</h1>
                 <p className="blog-content">
-                In a world filled with instant gratification and quick fixes</p>
+                In a world filled with instant gratification and quick fixes, investing may seem like a daunting and distant concept. 
+                </p>
 
         </main>
         </>
