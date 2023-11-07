4b4baf00aae7c99e39a4f9811c53587a6149669a
marionlouis1 <marionlouis1@gmail.com>
2023-11-07 15:58:53 +0100

    rmove next et vercel svg

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Script from 'next/script';
 import Calculator from '../components/calculator/calculator';
 import Header from '../components/header';
 import Footer from '../components/footer';
