d3d3b23f99f9023e55fce27eef927d2e567e5467
marionlouis1 <marionlouis1@gmail.com>
2023-11-11 11:51:30 +0100

    add 2 Link to footer

--- components/footer.js
+++ components/footer.js
@@ -5,9 +5,9 @@ const Footer = () => {
   return (
     <div className="bg-white text-black py-5">
       <div className="container mx-auto flex justify-between">
-        <p className="text-lg">
+        <Link  href="nextjs.org" className="text-lg">
           Powered by <u>Next.js</u>
-        </p>
+        </Link>
         <Link href="https://twitter.com/louismrn0" className="text-lg">
           Created By <u>LM</u>.
         </Link>
