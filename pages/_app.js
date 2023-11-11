c870575f73e8d12bef302e995311a2569fe1b317
marionlouis1 <marionlouis1@gmail.com>
2023-11-11 10:49:06 +0100

    fix logo4

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ export default function App({ Component, pageProps }) {
     return (
         <>
         <Head>
-        <link rel="icon" href="../public/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
     <Component {...pageProps} />
     <Analytics/>
